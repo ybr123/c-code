967ef384b745f3549ae8207bbfeaa5e73277fa47
ybr123 <73328565+ybr123@users.noreply.github.com>
2020-10-25 10:23:25 +0800

    Add files via upload

---
+++ homework2.cpp
@@ -0,0 +1,40 @@
+﻿
+
+
+#include <stdio.h>
+#include<stdlib.h>
+int main()
+{
+    printf("请输入三个正数");
+    double a, b, c, d;
+    scanf_s("%lf%lf%lf", &a, &b, &c);
+    if (c < a||c<b)
+    {if(c<a)
+    {
+        d = c;
+        c = a;
+        a = d;
+
+    }
+    else
+    {
+        d = c;
+        c = b;
+        b = d;
+    }
+    }
+    
+        if ((a + b) > c)
+        {
+            printf("能组成三角形\n");
+        }
+        else
+        {
+            printf("不能组成三角形\n");
+        }
+    
+    system("pause");
+    return 0;
+
+}
+
