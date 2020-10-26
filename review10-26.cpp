5316230b9404f2e2cc24bd876781a48da63b484e
ybr123 <73328565+ybr123@users.noreply.github.com>
2020-10-26 20:36:50 +0800

    Add files via upload

---
+++ review10-26.cpp
@@ -0,0 +1,31 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	int a[5], i, j, c;
+	for (i = 0; i < 5; i++)
+		scanf_s("%d",&a[i]);
+	for (i = 0; i < 5; i++)
+		for(j=0;j<5-i-1;j++)
+			if(a[j]>a[j+1])
+			{
+				c = a[j]; a[j] = a[j + 1]; a[j + 1] = c;
+
+			}
+		
+		
+		
+		
+		
+		for(i=0;i<5;i++)
+		printf("a[%d]=%d\n", i, a[i]);
+
+
+
+
+
+
+
+	return 0;
+}
